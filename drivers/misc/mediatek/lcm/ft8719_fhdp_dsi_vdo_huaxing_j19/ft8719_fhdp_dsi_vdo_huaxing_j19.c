239af2131267dd4479f3ba091f66a995d3ffb960
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-09-16 23:15:10 +0000

    drivers/misc/mediatek/lcm: esd_check_enable = 0

--- drivers/misc/mediatek/lcm/ft8719_fhdp_dsi_vdo_huaxing_j19/ft8719_fhdp_dsi_vdo_huaxing_j19.c
+++ drivers/misc/mediatek/lcm/ft8719_fhdp_dsi_vdo_huaxing_j19/ft8719_fhdp_dsi_vdo_huaxing_j19.c
@@ -285,8 +285,8 @@ static void lcm_get_params(struct LCM_PARAMS *params)
 	params->dsi.cont_clock = 1;
 #endif
 
-	params->dsi.esd_check_enable = 1;
-	params->dsi.customization_esd_check_enable = 1;
+	params->dsi.esd_check_enable = 0;
+	params->dsi.customization_esd_check_enable = 0;
 	params->dsi.lcm_esd_check_table[0].cmd = 0x0A;
 	params->dsi.lcm_esd_check_table[0].count = 1;
 	params->dsi.lcm_esd_check_table[0].para_list[0] = 0x9C;
