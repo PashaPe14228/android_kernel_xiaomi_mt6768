f46f43fb54d429ab831168d461472026c2cdcac9
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-11-13 08:56:01 +0000

    wip 2

--- drivers/misc/mediatek/base/power/spm/common/mtk_sleep.c
+++ drivers/misc/mediatek/base/power/spm/common/mtk_sleep.c
@@ -234,19 +234,18 @@ static int slp_suspend_ops_enter(suspend_state_t state)
 #if SLP_SLEEP_DPIDLE_EN
 #if defined(CONFIG_MTK_SND_SOC_NEW_ARCH) \
 || defined(CONFIG_SND_SOC_MTK_SMART_PHONE)
-	if (slp_ck26m_on | fm_radio_is_playing) {
+	if (slp_ck26m_on | fm_radio_is_playing)
 #else
-	if (slp_ck26m_on) {
+	if (slp_ck26m_on)
 #endif /* CONFIG_MTK_SND_SOC_NEW_ARCH */
 		slp_wake_reason = spm_go_to_sleep_ex(
 			SPM_SUSPEND_PLAT_SLP_DP);
 		slp_dp_cnt[smp_processor_id()]++;
-	} else {
+	else
 #endif
 		mtk_suspend_cond_info();
 
 		slp_wake_reason = spm_go_to_sleep_ex(0);
-	}
 
 	mcdi_task_pause(false);
 
